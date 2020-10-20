1a99d13bdcd6661074eddd97ec9565eda08cc41b
Luis Machado <lmachado@aquawm.com>
2020-10-20 14:39:49 -0300

    Visualización de quien esta logueado en APX

---
+++ AquaBackend/Controllers/APXUserSessionsController.cs
@@ -0,0 +1,42 @@
+﻿using AquaBackend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+using System.Data;
+
+namespace AquaBackend.Controllers
+{
+    public class APXUserSessionsController : Controller
+    {
+        // GET: APXUserSessions
+        public IActionResult Index()
+        {
+            List<APXUserSession> UserSessions = new List<APXUserSession>();
+
+            using (SqlConnection sqlConnection = new SqlConnection("data source=192.168.1.7;Database=APXFirm;User ID=lmachado;Password=Aq.Sql.2020"))
+            {
+                using var cmd = new SqlCommand()
+                {
+                    CommandText = "SELECT SessionID,FullName,Created,LastTouched FROM APXFirm.AdvApp.vUserSession",
+                    CommandType = CommandType.Text,
+                    Connection = sqlConnection
+                };
+                sqlConnection.Open();
+
+                using var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    APXUserSession APXUserSession = new APXUserSession
+                    {
+                        SessionID = reader[0].ToString(),
+                        FullName = reader[1].ToString(),
+                        Created = reader[2].ToString(),
+                        LastTouched = reader[3].ToString()
+                    };
+                    UserSessions.Add(APXUserSession);
+                }
+            }
+            return View(UserSessions);
+        }
+    }
+}
---
+++ AquaBackend/Models/APXUserSession.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace AquaBackend.Models
+{
+    [NotMapped]
+    public class APXUserSession
+    {
+        public String SessionID { get; set; }
+        [DisplayName("Nombre")]
+        public String FullName { get; set; }
+        [DisplayName("Inicio de sesión")]
+        public String Created { get; set; }
+        [DisplayName("Ultima Interacción")]
+        public String LastTouched { get; set; }
+    }
+}
